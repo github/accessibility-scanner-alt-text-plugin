8ff165eb08aedb1269af1ba1e72166d7fec73d75
Keenan Zhou <kzhou314@github.com>
2026-06-01 10:19:34 -0700

    Moved index.ts to folder root and mirrored eslint setup in scanner repo

--- eslint.config.js
+++ eslint.config.js
@@ -5,4 +5,17 @@ export default tseslint.config(
   {ignores: ['node_modules', 'dist', 'coverage', '.vitest']},
   ...tseslint.configs.recommended,
   prettier,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
 )
