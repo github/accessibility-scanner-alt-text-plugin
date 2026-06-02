be07b7f9677c3dc8df6ca38465b89e272d5b97ee
Keenan Zhou <kzhou314@github.com>
2026-06-02 12:18:40 -0700

    Add rule enforcing repeated alt text check

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -2,6 +2,7 @@ import type {Rule} from '../types.js'
 import {filenameAltText} from './filename-alt-text.js'
 import {vagueAltText} from './vagueAltText.js'
 import {missingAltText} from './missingAltText.js'
+import {repeatedAltText} from './repeatedAltText.js'
 
 // Append-only registry. Add a rule by importing it here and pushing it onto the array.
-export const allRules: Rule[] = [filenameAltText, vagueAltText, missingAltText]
+export const allRules: Rule[] = [filenameAltText, vagueAltText, missingAltText, repeatedAltText]
