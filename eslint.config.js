3f17decbe965f8e616eb3105eccaba9c733ec544
Keenan Zhou <kzhou314@github.com>
2026-06-05 10:49:11 -0700

    Add ESLint check-file rule to enforce kebab-case TypeScript filenames

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
+import checkFile from 'eslint-plugin-check-file'
 
 export default tseslint.config(
   {ignores: ['node_modules', 'dist', 'coverage', '.vitest']},
@@ -16,6 +17,13 @@ export default tseslint.config(
           varsIgnorePattern: '^_',
         },
       ],
+    },
+  },
+  {
+    files: ['src/**/*.ts', 'tests/**/*.ts'],
+    plugins: {'check-file': checkFile},
+    rules: {
+      'check-file/filename-naming-convention': ['error', {'**/*.ts': 'KEBAB_CASE'}, {ignoreMiddleExtensions: true}],
     },
   },
 )
